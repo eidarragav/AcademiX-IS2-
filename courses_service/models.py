af91955eec63da9d3b1018165848ae3dba50dc14
eidarragav <150448643+eidarragav@users.noreply.github.com>
2026-03-24 14:06:33 -0500

    feat: courses_service crud implementation (#1)

---
+++ courses_service/models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.String(300), nullable= False)
+    category = db.Column(db.String(100), nullable = False)
+    level = db.Column(db.String(100), nullable = False)
+    #user_id
+    instructor_id = db.Column(db.Integer, nullable = False)
+
