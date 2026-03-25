6e9afdb0782c3f6abf85ab4593e42b69e8a4c199
eidarragav <eidarragav@unal.edu.co>
2026-03-24 21:27:09 -0500

    feat: questions model

--- evaluations_service/models.py
+++ evaluations_service/models.py
@@ -9,3 +9,13 @@ class Exam(db.Model):
     title = db.Column(db.String(30), nullable= False)
     passing_score = db.Column(db.Integer, nullable = False)
 
+class Question(db.Model):
+    __tablename__ = 'questions'
+    id = db.Column(db.Integer, primary_key = True)
+    exam_id = db.Column(db.Integer, nullable = False)
+    text = db.Column(db.String(200), nullable= False)
+    option_a = db.Column(db.String(200), nullable = False)
+    option_b = db.Column(db.String(200), nullable = False)
+    option_c = db.Column(db.String(200), nullable = False)
+    option_d = db.Column(db.String(200), nullable = False)
+    correct_option = db.Column(db.String(2), nullable = False)
