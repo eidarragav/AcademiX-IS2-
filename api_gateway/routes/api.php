0ec9f588f852d5a20e0f32b274b4439b5685f326
eidarragav <eidarragav@unal.edu.co>
2026-03-28 15:45:09 -0500

    feat: enapi endpoints

--- api_gateway/routes/api.php
+++ api_gateway/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CoursesController;
+use App\Http\Controllers\EnrollmentsController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -23,3 +24,10 @@ Route::get("/courses", [CoursesController::class, 'index_courses']);
 Route::get("/courses/{id}", [CoursesController::class, 'index_course']);
 Route::put("/courses/{id}", [CoursesController::class, 'update_course']);
 Route::delete("/courses/{id}", [CoursesController::class, 'delete_course']);
+
+//Enrollments routes
+Route::post("/enrollments", [EnrollmentsController::class, 'create_enrollment'])->middleware('auth:sanctum');
+Route::get("/enrollments", [EnrollmentsController::class, 'index_enrollments']);
+Route::get("/enrollments/{id}", [EnrollmentsController::class, 'index_enrollment']);
+Route::put("/enrollments/{id}", [EnrollmentsController::class, 'update_enrollment']);
+Route::delete("/enrollments/{id}", [EnrollmentsController::class, 'delete_enrollment']);
