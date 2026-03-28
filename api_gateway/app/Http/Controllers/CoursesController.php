8b70d5a3b20cdfab63732065c58e07917d666db7
eidarragav <eidarragav@unal.edu.co>
2026-03-28 14:14:06 -0500

    feat: index courses and .env modification

--- api_gateway/app/Http/Controllers/CoursesController.php
+++ api_gateway/app/Http/Controllers/CoursesController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CoursesController extends Controller
 {
     public function create_course(Request $request){
-
+        
     }
 
     public function index_courses(){
+        $response = Http::withHeaders([
+            "Authorization" => env("services_token")
+        ])->get(env("courses_endpoint"));
 
+        return [
+            "status" => $response->status,
+            "body" => $response->body
+        ];
     }
 
     public function index_course(){
