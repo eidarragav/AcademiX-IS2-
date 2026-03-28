608c63df1b1c0ef0b5dee328e3fd68fb45f68b5a
eidarragav <eidarragav@unal.edu.co>
2026-03-28 15:31:12 -0500

    feat: empty crud functions

---
+++ api_gateway/app/Http/Controllers/EnrollmentsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EnrollmentsController extends Controller
+{
+    public function create_enrollment(Request $request){
+
+    }
+
+    public function index_enrollments(){
+
+    }
+
+    public function index_enrollment($id){
+
+    }
+    
+    public function update_enrollment($id){
+
+    }
+
+    public function delete_enrollment($id){
+
+    }
+}
+
+
--- enrollments_service/manage.py
+++ enrollments_service/manage.py
@@ -15,6 +15,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+
+    if len(sys.argv) >= 2 and sys.argv[1] == 'runserver' and len(sys.argv) == 2:
+        sys.argv.append('8080')
+
     execute_from_command_line(sys.argv)
 
 
