d8f2c8c72206e7086c494b55c14d630d7e28c39f
eidarragav <eidarragav@unal.edu.co>
2026-03-28 14:43:40 -0500

    feat: deadd courses routes to api file

--- api_gateway/routes/api.php
+++ api_gateway/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CoursesController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +15,11 @@ Route::post("/register", [AuthController::class, 'register']);
 Route::post("/login", [AuthController::class, 'login']);
 Route::post("/logout", [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::post("/restore_password", [AuthController::class, 'restore_password']);
+
+
+//Courses routes
+Route::post("/courses", [CoursesController::class, 'create_course']);
+Route::get("/courses", [CoursesController::class, 'index_courses']);
+Route::get("/courses/{id}", [CoursesController::class, 'index_course']);
+Route::put("/courses/{id}", [CoursesController::class, 'update_course']);
+Route::delete("/courses/{id}", [CoursesController::class, 'delete_course']);
