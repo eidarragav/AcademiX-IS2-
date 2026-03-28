fbf4cd4b40c3ea5912015fffda7ac0103086f15a
eidarragav <eidarragav@unal.edu.co>
2026-03-28 14:07:22 -0500

    basic crud empty functions

---
+++ api_gateway/app/Http/Controllers/CoursesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CoursesController extends Controller
+{
+    public function create_course(Request $request){
+
+    }
+
+    public function index_courses(){
+
+    }
+
+    public function index_course(){
+
+    }
+
+    public function update_course(Request $request, $id){
+
+    }
+
+    public function delete_course(Request $request, $id){
+
+    }
+}
