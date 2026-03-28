00db838f15129fb5416aad622bed554bf3ce82f9
eidarragav <eidarragav@unal.edu.co>
2026-03-28 14:38:49 -0500

    feat: update courses

--- api_gateway/app/Http/Controllers/CoursesController.php
+++ api_gateway/app/Http/Controllers/CoursesController.php
@@ -49,7 +49,21 @@ class CoursesController extends Controller
     }
 
     public function update_course(Request $request, $id){
+        $instructor = auth()->user();
 
+        $response = Http::withHeaders([
+            "Authorization" => env("services_token")
+        ])->put(env("courses_endpoint"."/".$id),[
+            "title" => $request->title,
+            "description" => $request->description,
+            "category" => $request->category,
+            "level" => $request->level,
+        ]);
+
+        return [
+            "status" => $response->status,
+            "body" => $response->body
+        ];
     }
 
     public function delete_course(Request $request, $id){
