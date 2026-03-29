2bf39f58992a5cbc2fa3d5b0937bab2d3df2c5c8
eidarragav <eidarragav@unal.edu.co>
2026-03-29 18:29:46 -0500

    feat: create empty crud methods

---
+++ api_gateway/app/Http/Controllers/ModuleController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ModuleController extends Controller
+{
+    public function create_module(Request $request){
+
+    }
+
+    public function index_modules(){
+
+    }
+
+    public function index_module($id){
+
+    }
+
+    public function update_module(Request $request, $id){
+
+    }
+
+    public function delete_module($id){
+        
+    }
+}
