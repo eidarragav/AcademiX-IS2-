83cba29fddccf98ad7bd63c60f923662a4fb1757
eidarragav <eidarragav@unal.edu.co>
2026-03-29 18:37:03 -0500

    feat: create module method

--- api_gateway/app/Http/Controllers/ModuleController.php
+++ api_gateway/app/Http/Controllers/ModuleController.php
@@ -8,7 +8,29 @@ use Illuminate\Support\Facades\Http;
 class ModuleController extends Controller
 {
     public function create_module(Request $request){
+        
+        $course_exist = Http::withHeaders([
+            "Authorization" => env("SERVICES_TOKEN")
+        ])->get(env("COURSES_ENDPOINT")."/".$request->course_id);
 
+        if($course_exist->status() == 404){
+            return [
+                "mensaje" => "el curso no existe"
+            ];
+        };
+
+        $response = Http::withHeaders([
+            "Authorization" => env("SERVICES_TOKEN")
+        ])->post(env("CONTENT_MODULES_ENDPOINT"),[
+            "course_id" => $request->course_id,
+            "title" => $request->title,
+            "description" => $request->description
+        ]);
+
+        return [
+            "status" => $response->status(),
+            "body" => $response->json()
+        ];
     }
 
     public function index_modules(){
