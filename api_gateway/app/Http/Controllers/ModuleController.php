06872ce55b5c81224deac63a76cc2527b674b6cf
eidarragav <eidarragav@unal.edu.co>
2026-03-29 18:37:51 -0500

    feat: update modules method

--- api_gateway/app/Http/Controllers/ModuleController.php
+++ api_gateway/app/Http/Controllers/ModuleController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Http;
 class ModuleController extends Controller
 {
     public function create_module(Request $request){
-        
+
         $course_exist = Http::withHeaders([
             "Authorization" => env("SERVICES_TOKEN")
         ])->get(env("COURSES_ENDPOINT")."/".$request->course_id);
@@ -56,7 +56,28 @@ class ModuleController extends Controller
     }
 
     public function update_module(Request $request, $id){
+        $course_exist = Http::withHeaders([
+            "Authorization" => env("SERVICES_TOKEN")
+        ])->get(env("COURSES_ENDPOINT")."/".$request->course_id);
 
+        if($course_exist->status() == 404){
+            return [
+                "mensaje" => "el curso no existe"
+            ];
+        };
+
+        $response = Http::withHeaders([
+            "Authorization" => env("SERVICES_TOKEN")
+        ])->put(env("CONTENT_MODULES_ENDPOINT"),[
+            "course_id" => $request->course_id,
+            "title" => $request->title,
+            "description" => $request->description
+        ]);
+
+        return [
+            "status" => $response->status(),
+            "body" => $response->json()
+        ];
     }
 
     public function delete_module($id){
