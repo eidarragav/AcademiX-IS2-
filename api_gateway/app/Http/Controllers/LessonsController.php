21e0e3836340560df002c368b25443e33ba90a5d
eidarragav <eidarragav@unal.edu.co>
2026-03-29 19:12:42 -0500

    fix: rename controller in plural

--- api_gateway/app/Http/Controllers/LessonsController.php
+++ api_gateway/app/Http/Controllers/LessonsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
-class LessonController extends Controller
+class LessonsController extends Controller
 {
     public function index_lesson($id){
 
@@ -24,6 +24,6 @@ class LessonController extends Controller
     } 
     
     public function delete_lesson($id){
-        
+
     }
 }
