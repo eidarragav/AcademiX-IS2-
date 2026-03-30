afd071314abc2dfb9060eb7f0a37b69f34ae3880
eidarragav <eidarragav@unal.edu.co>
2026-03-29 19:11:55 -0500

    feat: empty crud functions

---
+++ api_gateway/app/Http/Controllers/LessonController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class LessonController extends Controller
+{
+    public function index_lesson($id){
+
+    }
+
+    public function index_lessons(){
+
+    }
+
+    public function create_lesson(Request $request){
+
+    }
+
+    public function update_lesson(Request $request, $id){
+
+    } 
+    
+    public function delete_lesson($id){
+        
+    }
+}
