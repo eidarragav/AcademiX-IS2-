93574b029a20552a4a4fcf264e227b0ed8d2eb9a
eidarragav <eidarragav@unal.edu.co>
2026-03-31 09:21:47 -0500

    feat: create exam method

--- api_gateway/app/Http/Controllers/ExamController.php
+++ api_gateway/app/Http/Controllers/ExamController.php
@@ -30,7 +30,29 @@ class ExamController extends Controller
     }
 
     public function create_exam(Request $request){
+        
+        $course_exist = Http::withHeaders([
+            "Authorization" => env("SERVICES_TOKEN")
+        ])->get(env("COURSES_ENDPOINT")."/".$request->course_id);
 
+        if($course_exist->status() == 404){
+            return [
+                "mensaje" => "el curso no existe"
+            ];
+        };
+
+        $response = Http::withHeaders([
+            "Authorization" => env("SERVICES_TOKEN")
+        ])->post(env("EXAMS_EVALUATIONS_ENDPOINT"),[
+            "title" => $request->title,
+            "course_id" => $request->course_id,
+            "passing_score" => $request->passing_score
+        ]);
+
+        return [
+            "status" => $response->status(),
+            "body" => $response->json()
+        ]; 
     }
 
     public function update_exam(Request $request, $id){
