8b88482bd70c61c7b5d2745dc12e8b8b071cb0dd
eidarragav <eidarragav@unal.edu.co>
2026-03-31 09:19:01 -0500

    feat: index one exam method

--- api_gateway/app/Http/Controllers/ExamController.php
+++ api_gateway/app/Http/Controllers/ExamController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ExamController extends Controller
 {
     public function index_exam($id){
-
+        
     }
 
     public function index_exams(){
+        $response = Http::withHeaders([
+            "Authorization" => env("SERVICES_TOKEN")
+        ])->get(env("EXAMS_EVALUATIONS_ENDPOINT"));
 
+        return [
+            "status" => $response->status(),
+            "body" => $response->json()
+        ];
     }
 
     public function create_exam(Request $request){
@@ -23,6 +31,6 @@ class ExamController extends Controller
     }
 
     public function delete_exam($id){
-        
+
     }
 }
