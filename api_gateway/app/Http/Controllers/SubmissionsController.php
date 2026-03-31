83534f379687f40ef2e6b8c754709f98ad4ba0b9
eidarragav <eidarragav@unal.edu.co>
2026-03-31 10:18:07 -0500

    feat: empty crud methods

---
+++ api_gateway/app/Http/Controllers/SubmissionsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class SubmissionsController extends Controller
+{
+    public function index_submission($id){
+
+    }
+
+    public function index_submissions(){
+        
+    }
+
+    public function create_submission(Request $request){
+
+    }
+
+    public function update_submission(Request $request, $id){
+
+    }
+
+    public function delete_submission($id){
+
+    }
+
+
+}
