38bc5570569c018200b648ffb984f8e2b7837311
eidarragav <eidarragav@unal.edu.co>
2026-03-31 10:22:01 -0500

    feat: index submissions method

--- api_gateway/app/Http/Controllers/SubmissionsController.php
+++ api_gateway/app/Http/Controllers/SubmissionsController.php
@@ -12,7 +12,14 @@ class SubmissionsController extends Controller
     }
 
     public function index_submissions(){
-        
+        $response = Http::withHeaders([
+            "Authorization" => env("SERVICES_TOKEN")
+        ])->get(env("SUBMISSIONS_EVALUATIONS_ENDPOINT"));
+
+        return [
+            "status" => $response->status(),
+            "body" => $response->json()
+        ];
     }
 
     public function create_submission(Request $request){
