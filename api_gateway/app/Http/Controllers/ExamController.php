145ccf3881d256d38cf092c9041ca7464cf0ede9
eidarragav <eidarragav@unal.edu.co>
2026-03-31 09:15:12 -0500

    feat: empty crud exams functions

---
+++ api_gateway/app/Http/Controllers/ExamController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExamController extends Controller
+{
+    public function index_exam($id){
+
+    }
+
+    public function index_exams(){
+
+    }
+
+    public function create_exam(Request $request){
+
+    }
+
+    public function update_exam(Request $request, $id){
+
+    }
+
+    public function delete_exam($id){
+        
+    }
+}
