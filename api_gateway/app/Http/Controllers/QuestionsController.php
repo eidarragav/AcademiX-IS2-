f41606771a2a7ad7fd9ba325a2c03a2fd55ea373
eidarragav <eidarragav@unal.edu.co>
2026-03-31 09:43:34 -0500

    feat: empty crud functions

---
+++ api_gateway/app/Http/Controllers/QuestionsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionsController extends Controller
+{
+    public function index_question(){
+
+    }
+
+    public function index_questions(){
+
+    }
+
+    public function create_question(){
+
+    }
+
+    public function update_question(){
+
+    }
+
+    public function delete_question(){
+        
+    }
+}
