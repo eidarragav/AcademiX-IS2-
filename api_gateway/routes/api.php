d0d5b6570caafc358bd7bd01923df6f2ab3cf01b
eidarragav <eidarragav@unal.edu.co>
2026-04-03 11:35:14 -0500

    feat: apply sanctum middleware to every endpoint

--- api_gateway/routes/api.php
+++ api_gateway/routes/api.php
@@ -16,58 +16,64 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-//Autenticacion
-Route::post("/register", [AuthController::class, 'register']);
-Route::post("/login", [AuthController::class, 'login']);
-Route::post("/logout", [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::post("/restore_password", [AuthController::class, 'restore_password']);
+//Todos requieren autenticacion del usuario para funcionar
+Route::middleware('auth:sanctum')->group(function () {
+   //Autenticacion
+    Route::post("/register", [AuthController::class, 'register']);
+    Route::post("/login", [AuthController::class, 'login']);
+    Route::post("/logout", [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::post("/restore_password", [AuthController::class, 'restore_password']);
 
 
-//Courses routes
-Route::post("/courses", [CoursesController::class, 'create_course'])->middleware('auth:sanctum');
-Route::get("/courses", [CoursesController::class, 'index_courses']);
-Route::get("/courses/{id}", [CoursesController::class, 'index_course']);
-Route::put("/courses/{id}", [CoursesController::class, 'update_course']);
-Route::delete("/courses/{id}", [CoursesController::class, 'delete_course']);
+    //Courses routes
+    Route::post("/courses", [CoursesController::class, 'create_course'])->middleware('auth:sanctum');
+    Route::get("/courses", [CoursesController::class, 'index_courses']);
+    Route::get("/courses/{id}", [CoursesController::class, 'index_course']);
+    Route::put("/courses/{id}", [CoursesController::class, 'update_course']);
+    Route::delete("/courses/{id}", [CoursesController::class, 'delete_course']);
 
-//Enrollments routes
-Route::post("/enrollments", [EnrollmentsController::class, 'create_enrollment'])->middleware('auth:sanctum');
-Route::get("/enrollments", [EnrollmentsController::class, 'index_enrollments']);
-Route::get("/enrollments/{id}", [EnrollmentsController::class, 'index_enrollment']);
-Route::put("/enrollments/{id}", [EnrollmentsController::class, 'update_enrollment'])->middleware('auth:sanctum');;
-Route::delete("/enrollments/{id}", [EnrollmentsController::class, 'delete_enrollment']);
+    //Enrollments routes
+    Route::post("/enrollments", [EnrollmentsController::class, 'create_enrollment'])->middleware('auth:sanctum');
+    Route::get("/enrollments", [EnrollmentsController::class, 'index_enrollments']);
+    Route::get("/enrollments/{id}", [EnrollmentsController::class, 'index_enrollment']);
+    Route::put("/enrollments/{id}", [EnrollmentsController::class, 'update_enrollment'])->middleware('auth:sanctum');;
+    Route::delete("/enrollments/{id}", [EnrollmentsController::class, 'delete_enrollment']);
 
-//Modules routes
-Route::post("/modules", [ModulesController::class, 'create_module']);
-Route::get("/modules", [ModulesController::class, 'index_modules']);
-Route::get("/modules/{id}", [ModulesController::class, 'index_module']);
-Route::put("/modules/{id}", [ModulesController::class, 'update_module']);
-Route::delete("/modules/{id}", [ModulesController::class, 'delete_module']);
+    //Modules routes
+    Route::post("/modules", [ModulesController::class, 'create_module']);
+    Route::get("/modules", [ModulesController::class, 'index_modules']);
+    Route::get("/modules/{id}", [ModulesController::class, 'index_module']);
+    Route::put("/modules/{id}", [ModulesController::class, 'update_module']);
+    Route::delete("/modules/{id}", [ModulesController::class, 'delete_module']);
 
-//Lesson routes
-Route::post("/lessons", [LessonsController::class, 'create_lesson']);
-Route::get("/lessons", [LessonsController::class, 'index_lessons']);
-Route::get("/lessons/{id}", [LessonsController::class, 'index_lesson']);
-Route::put("/lessons/{id}", [LessonsController::class, 'update_lesson']);
-Route::delete("/lessons/{id}", [LessonsController::class, 'delete_lesson']);
+    //Lesson routes
+    Route::post("/lessons", [LessonsController::class, 'create_lesson']);
+    Route::get("/lessons", [LessonsController::class, 'index_lessons']);
+    Route::get("/lessons/{id}", [LessonsController::class, 'index_lesson']);
+    Route::put("/lessons/{id}", [LessonsController::class, 'update_lesson']);
+    Route::delete("/lessons/{id}", [LessonsController::class, 'delete_lesson']);
 
-//Exams routes
-Route::post("/exams", [ExamController::class, 'create_exam']);
-Route::get("/exams", [ExamController::class, 'index_exams']);
-Route::get("/exams/{id}", [ExamController::class, 'index_exam']);
-Route::put("/exams/{id}", [ExamController::class, 'update_exam']);
-Route::delete("/exams/{id}", [ExamController::class, 'delete_exam']);
+    //Exams routes
+    Route::post("/exams", [ExamController::class, 'create_exam']);
+    Route::get("/exams", [ExamController::class, 'index_exams']);
+    Route::get("/exams/{id}", [ExamController::class, 'index_exam']);
+    Route::put("/exams/{id}", [ExamController::class, 'update_exam']);
+    Route::delete("/exams/{id}", [ExamController::class, 'delete_exam']);
 
-//Questions routes
-Route::post("/questions", [QuestionsController::class, 'create_question']);
-Route::get("/questions", [QuestionsController::class, 'index_questions']);
-Route::get("/questions/{id}", [QuestionsController::class, 'index_question']);
-Route::put("/questions/{id}", [QuestionsController::class, 'update_question']);
-Route::delete("/questions/{id}", [QuestionsController::class, 'delete_question']);
+    //Questions routes
+    Route::post("/questions", [QuestionsController::class, 'create_question']);
+    Route::get("/questions", [QuestionsController::class, 'index_questions']);
+    Route::get("/questions/{id}", [QuestionsController::class, 'index_question']);
+    Route::put("/questions/{id}", [QuestionsController::class, 'update_question']);
+    Route::delete("/questions/{id}", [QuestionsController::class, 'delete_question']);
 
-//Questions routes
-Route::post("/submissions", [SubmissionsController::class, 'create_submission'])->middleware('auth:sanctum');
-Route::get("/submissions", [SubmissionsController::class, 'index_submissions']);
-Route::get("/submissions/{id}", [SubmissionsController::class, 'index_submission']);
-Route::put("/submissions/{id}", [SubmissionsController::class, 'update_submission'])->middleware('auth:sanctum');
-Route::delete("/submissions/{id}", [SubmissionsController::class, 'delete_submission']);
+    //Questions routes
+    Route::post("/submissions", [SubmissionsController::class, 'create_submission'])->middleware('auth:sanctum');
+    Route::get("/submissions", [SubmissionsController::class, 'index_submissions']);
+    Route::get("/submissions/{id}", [SubmissionsController::class, 'index_submission']);
+    Route::put("/submissions/{id}", [SubmissionsController::class, 'update_submission'])->middleware('auth:sanctum');
+    Route::delete("/submissions/{id}", [SubmissionsController::class, 'delete_submission']); 
+});
+
+
+
